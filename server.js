25876bd1c71b602139385035904ed58dbef61672
Melanie DeWitt <melaniebd1986@gmail.com>
2021-09-13 18:06:58 -0600

    added server function stubs

--- server.js
+++ server.js
@@ -9,3 +9,22 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`listening at port ${port}`);
 });
+
+
+
+
+app.get('/user', (req, res) => {
+  // get druid level
+})
+
+app.get('/shapes', (req, res) => {
+  // get current shape data
+})
+
+app.post('/shapes', (req, res) => {
+  // add a new shape from the client side
+})
+
+app.patch('/shapes', (req, res) => {
+  // update a beast that the druid has seen
+})
